946a81e3b275915883912746f59a1a6b0719f753
Asif Rahman <arahman22@apple.com>
2025-08-16 11:42:28 -0400

    refactored with modular redesign

--- autostore/__init__.py
+++ autostore/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.1.10"
+__version__ = "0.1.11"
 
-from autostore.autostore import AutoStore, Options, load_dotenv, config, setup_logging  # noqa: F401
+from autostore.autostore import AutoStore
